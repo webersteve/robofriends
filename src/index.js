c257249c5606c03e586659ce685d41111518f9b4
webersteve <weberwcr@outlook.com>
2022-03-25 20:49:10 -0400

    added App.js and SearchBox.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
-import CardList from './CardList';
-import {robots} from './robots';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
